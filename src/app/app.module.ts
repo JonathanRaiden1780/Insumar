7a15bffbb9d9a77c8a18b083f46bbd762a98a802
jonathan huerta <mejahv_hs@hotmail.com>
2018-11-09 13:51:47 -0600

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { StartComponent } from './components/start/start.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomePageComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    StartComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
