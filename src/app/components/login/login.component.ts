ccd833539fd5ad4170bb07614510485d8c53cf3f
jonathan huerta <mejahv_hs@hotmail.com>
2018-11-14 15:02:59 -0600

    coreccion login by olvera

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faKey, faUser, faDolly } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -11,5 +12,9 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  faKey = faKey;
+  faUser = faUser;
+  faDolly = faDolly;
 
 }
