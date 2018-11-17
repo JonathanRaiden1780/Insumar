ee59a68c34dd14929ce7f3af36e204e3f9bb6dcc
jonathan huerta <mejahv_hs@hotmail.com>
2018-11-16 21:36:44 -0600

    deploy2

---
+++ src/app/components/sucursal/sucursal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sucursal',
+  templateUrl: './sucursal.component.html',
+  styleUrls: ['./sucursal.component.css']
+})
+export class SucursalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
