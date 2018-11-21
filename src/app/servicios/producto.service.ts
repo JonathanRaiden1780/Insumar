0c371f5b77b432087c6cd39ba24b8f892080a911
jonathan huerta <mejahv_hs@hotmail.com>
2018-11-21 16:42:41 -0600

    cambio

--- src/app/servicios/producto.service.ts
+++ src/app/servicios/producto.service.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@angular/core';
-import {Productos} from '../Models/Productos'
+import {ProductosInterface} from '../Models/Productos';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
+ProductosCollection: AngularFirestoreCollection<ProductosInterface>;
+ProductosDoc: AngularFirestoreDocument<ProductosInterface>;
+Productos: Observable<ProductosInterface[]>;
+Producto: Observable<ProductosInterface>;
+  constructor(
+    private afs: AngularFirestore) {
+      this.ProductosCollection = this.afs.collection('Productos', ref => ref);
+     }
 
-  constructor() { }
+    deleteProducto(){}
+    ActProducto(Producto: ProductosInterface){
+      this.ProductosCollection.add(Producto);
+    }
+    getOneProducto(idprodu: string){
+      this.ProductosDoc = this.afs.doc<ProductosInterface>('productos/${idprodu}');
+      this.Producto = this.ProductosDoc.snapshotChanges().pipe(map(action =>{
+        if(action.payload.exists === false){
+          return null;
+        }else{
+          const data = action.payload.data() as ProductosInterface;
+          data.idprod = action.payload.id;
+          
+        }))
+    }
+    getAllProducto():Observable<ProductosInterface[]>{
+      this.Productos = this.ProductosCollection.snapshotChanges()
+      .pipe(map(changes => {
+        return changes.map(action => {
+          const data = action.payload.doc.data() as ProductosInterface;
+          data.idprod = action.payload.doc.id;
+          return data;
+        });
+      }));
+      return this.Productos;
+    }
 }
