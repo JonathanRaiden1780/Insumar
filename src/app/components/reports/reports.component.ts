88b29aa1084dc37a5de9de3c8b65d6c998fa076e
jonathan huerta <mejahv_hs@hotmail.com>
2018-12-17 21:21:44 -0600

    imprimir reportes

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { ProductoService } from 'src/app/servicios/producto.service';
+import { ControlService } from 'src/app/servicios/control.service';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import { formatDate } from '@angular/common';
+ 
 
 @Component({
   selector: 'app-reports',
@@ -7,7 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('content') content: ElementRef;
+  listadoProductos: any;
+  public downloadPDF(){
+    let doc = new jsPDF();
+
+    let specialElementHandlers ={
+      '#editor': function(element, renderer){
+        return true;
+      }
+    };
+    let content = this.content.nativeElement;
+    
+    doc.fromHTML(content.innerHTML, 15,15, {
+      'width': 300,
+      'elementHandlers': specialElementHandlers,
+      'height': 400
+    });
+    
+    
+    doc.save('Reporte' + formatDate(new Date(),'dd/MM/yyyy hh:mm:ss a','en') + '.pdf');
+  }
+  constructor(
+    private authService: AuthService,
+    public productos: ControlService
+    
+  ) {
+    this.listadoProductos = productos.getAllCoentrada();
+   }
 
   ngOnInit() {
   }
